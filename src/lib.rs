86bb50d6a37c2cc7464449bd194e6a267c7fae78
Maxwell Henderson <mxwhenderson@gmail.com>
2024-03-23 07:40:59 -0700

    Add worktree removal
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

---
+++ src/lib.rs
@@ -0,0 +1,62 @@
+use std::error::Error;
+use std::fmt;
+use std::path::Path;
+use std::process::Command;
+
+pub struct Worktree;
+
+impl Worktree {
+    pub fn remove_worktree(
+        filename: String,
+        branch_name: Option<String>,
+    ) -> Result<(), Box<dyn Error>> {
+        let file_path = Path::new(&filename);
+
+        if !file_path.exists() {
+            return Err(Box::new(WorktreeError(format!(
+                "File not found: {}",
+                filename
+            ))));
+        }
+
+        println!("Removing directory {filename}");
+
+        std::fs::remove_dir_all(file_path)?;
+
+        Self::spawn_cmd("git worktree prune".to_string())?;
+
+        if let Some(branch) = branch_name {
+            Self::spawn_cmd(format!("git branch -D {branch}"))?;
+        }
+
+        return Ok(());
+    }
+
+    fn spawn_cmd(command_name: String) -> Result<(), Box<dyn Error>> {
+        println!("Running: {command_name}");
+
+        let mut command = Command::new("sh").arg("-c").arg(&command_name).spawn()?;
+
+        let status = command.wait()?;
+
+        if !status.success() {
+            return Err(Box::new(WorktreeError(format!(
+                "Failed to run command '{}' with exit code {}",
+                command_name, status
+            ))));
+        }
+
+        return Ok(());
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct WorktreeError(String);
+
+impl fmt::Display for WorktreeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Worktree error: {}", self.0)
+    }
+}
+
+impl std::error::Error for WorktreeError {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
+use worktree::Worktree;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.get(1).unwrap() == "remove" {
+        let filename = args.get(2).unwrap();
+        let branch = args.get(3);
+
+        Worktree::remove_worktree(filename.to_string(), branch.cloned()).unwrap();
+    }
 }
