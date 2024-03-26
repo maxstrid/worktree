3692bee1128143b26742bdc6262850b36f424361
Maxwell Henderson <mxwhenderson@gmail.com>
2024-03-26 06:48:57 -0700

    Start implementing the ArgumentParser
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -1,17 +1,30 @@
-use std::fmt::Debug;
+use std::{collections::HashMap, error::Error, hash::Hash, hash::Hasher};
 
-pub trait MainArgument<T>
-where
-    String: std::convert::From<T>,
-    T: Clone + std::fmt::Debug,
-{
-    type ArgumentName;
-
-    fn handle_sub_args(sub_args: Vec<SubArgument>);
-    fn register_sub_arg(sub_arg: SubArgument);
+pub trait MainArgument {
+    fn name(&self) -> String;
+    fn handle_sub_args(&self, sub_args: Vec<SubArgument>);
+    fn register_sub_arg(&self, sub_arg: SubArgument);
+    fn sub_args(&self) -> Vec<SubArgument>;
 }
 
-trait Argument: Debug {
+impl Hash for Box<dyn MainArgument> {
+    fn hash<H>(&self, state: &mut H)
+    where
+        H: Hasher,
+    {
+        self.name().hash(state)
+    }
+}
+
+impl PartialEq for Box<dyn MainArgument> {
+    fn eq(&self, other: &Box<dyn MainArgument>) -> bool {
+        self.name() == other.name()
+    }
+}
+
+impl Eq for Box<dyn MainArgument> {}
+
+trait Argument {
     fn default() -> Self
     where
         Self: Sized;
@@ -19,7 +32,7 @@ trait Argument: Debug {
     fn from_string(&self, value: &str) -> Box<dyn Argument>;
 }
 
-impl<T: From<String> + Debug + Default + 'static> Argument for T {
+impl<T: From<String> + Default + 'static> Argument for T {
     fn default() -> Self {
         return T::default();
     }
@@ -28,7 +41,6 @@ impl<T: From<String> + Debug + Default + 'static> Argument for T {
     }
 }
 
-#[derive(Debug)]
 pub struct SubArgument {
     name: String,
     default_value: Option<Box<dyn Argument>>,
@@ -36,7 +48,22 @@ pub struct SubArgument {
     short_name: Option<String>,
 }
 
-#[derive(Debug)]
-struct ArgumentParser {
-    sub_arguments: Vec<SubArgument>,
+struct ArgumentParser<'a> {
+    sub_arguments: HashMap<&'a Box<dyn MainArgument>, Vec<SubArgument>>,
+}
+
+impl<'a> ArgumentParser<'a> {
+    pub fn new() -> Self {
+        return ArgumentParser {
+            sub_arguments: HashMap::new(),
+        };
+    }
+
+    pub fn register_main_arg(&mut self, main_arg: &'a Box<dyn MainArgument>) {
+        self.sub_arguments.insert(main_arg, main_arg.sub_args());
+    }
+
+    pub fn parse(&self, args: Vec<String>) -> Result<(), Box<dyn Error>> {
+        todo!()
+    }
 }
